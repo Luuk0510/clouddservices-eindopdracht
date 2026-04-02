470eac43aeb97c4bdae44bf920616b145cd6f556
Luuk <0510luuk@gmail.com>
2026-04-02 22:34:06 +0200

    add config files

--- target-service/server.js
+++ target-service/server.js
@@ -1,16 +1,12 @@
 var app = require('./app');
-var mongoose = require('mongoose');
+var connectDatabase = require('./config/database');
+var env = require('./config/env');
 
-var port = process.env.PORT || 3002;
-var mongoUri = process.env.MONGODB_URI || 'mongodb://mongodb:27017/photo-prestige';
-
-mongoose.connect(mongoUri).then(function() {
-  console.log('Target service connected to MongoDB');
-
-  app.listen(port, function() {
-    console.log('Target service listening on port ' + port);
+connectDatabase(env.mongoUri).then(function() {
+  app.listen(env.port, function() {
+    console.log('Target service listening on port ' + env.port);
   });
 }).catch(function(error) {
-  console.error('Failed to connect to MongoDB', error);
+  console.error('Failed to start target service', error);
   process.exit(1);
 });
